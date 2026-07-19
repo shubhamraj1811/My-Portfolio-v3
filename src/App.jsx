29216677462207991563516ee3914be53ed7d3f9
Shubham Raj <ershubhcsofficial@gmail.com>
2026-07-19 19:45:02 +0530

    Portfolio Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GradientBackground from './components/GradientBackground'
+
+function App() {
+    return (
+        <div className="min-h-screen text-white">
+            <GradientBackground />
+            <h1 className="text-4xl font-bold p-8">Portfolio setup working ✅</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/GradientBackground.jsx
@@ -0,0 +1,11 @@
+function GradientBackground() {
+    return (
+        <div className="gradient-bg">
+            <div className="gradient-blob blob-1"></div>
+            <div className="gradient-blob blob-2"></div>
+            <div className="gradient-blob blob-3"></div>
+        </div>
+    )
+}
+
+export default GradientBackground
